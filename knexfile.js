6df4173d3e2636b22d936448461f4e28ebfc2a58
Ryan Galvão <60650064+ryangalvaogp@users.noreply.github.com>
2021-02-23 03:42:30 -0300

    Configurando conexão com banco de dados

---
+++ knexfile.js
@@ -0,0 +1,58 @@
+// modifique com as suas configurações
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: { 
+      port: '3306',
+      database: 'sql10364911',
+      host: 'sql10.freemysqlhosting.net',
+      user: 'sql10364911',
+      password: 'kNfJ9KuKuk',
+    }, 
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {
+    client: 'mysql',
+    connection: { 
+      port: '3308',
+      database: 'venalvo',
+      host:'127.0.0.1',
+      user: 'root',
+      password: '',
+    },
+    debug: true
+    ,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'mysql',
+    connection: { 
+      port: '3306',
+      database: 'sql10364911',
+      host:'sql10.freemysqlhosting.net',
+      user: 'sql10364911',
+      password: 'kNfJ9KuKuk',
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
